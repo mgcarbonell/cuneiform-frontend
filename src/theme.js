47401e8923c1de24e3fabe3e203482009089f2c9
dcblair <devin.c.blair@gmail.com>
2020-11-18 12:31:28 -0800

    create material theme

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    light: "#d7f9f1",
+    main: "#b2edf2",
+    dark: "#C1B4B6",
+    contrastText: "#fff",
+  },
+  secondary: {
+    light: ,
+    main: ,
+    dark: ,
+    contrastText: "#fff",
+  }
+})
