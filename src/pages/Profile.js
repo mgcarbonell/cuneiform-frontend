ac0b5ae4fef561a958be9fdb9d4eb463f9451757
mgcarbonell <mgcarbonell@gmail.com>
2020-11-20 15:40:38 -0800

    Remove userId from profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,11 +4,7 @@ import UserEntries from '../components/UserEntries';
 const Profile = props => {
   return (
     <>
-<<<<<<< HEAD
-=======
     <UserEntries />
-    <h1>Profile of user with ID { props.currentUser }</h1>
->>>>>>> submain
     <h1>Profile of user with ID { props.currentUsername }</h1>
     </>
   )
