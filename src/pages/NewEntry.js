876a86432dfe4f4c4026b251d79467bfe4927887
dcblair <devin.c.blair@gmail.com>
2020-11-19 21:06:58 -0800

    minor tweaks

--- src/pages/NewEntry.js
+++ src/pages/NewEntry.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Container, TextField } from '@material-ui/core';
 import EntryModel from '../models/entry';
-import { SentimentSatisfied } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
