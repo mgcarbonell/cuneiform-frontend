ed1f4e2fbe9d9c74de236e7aa139bae88c532bc7
dcblair <devin.c.blair@gmail.com>
2020-11-28 23:19:50 -0800

    pull prep

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,18 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NewEntryDialog from './NewEntryDialog';
 import NewEntryForm from './NewEntryForm';
-import { AppBar, Toolbar, Button, IconButton, MenuItem, Menu } from '@material-ui/core';
+import { 
+              AppBar, 
+              Toolbar, 
+              Button, 
+              IconButton, 
+              MenuItem, 
+              Menu,
+              Switch, 
+              Grid } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle'
+import Brightness7Icon from '@material-ui/icons/Brightness7'
+import Brightness4Icon from '@material-ui/icons/Brightness4'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,22 +21,17 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
 }));
-
 const Navbar = (props) => {
-
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
   const open = Boolean(anchorEl);
-
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null)
   };
-
 
   return (
     <div className={classes.root}>
@@ -38,81 +43,87 @@ const Navbar = (props) => {
               </Button>
             </Link>
             <Grid
-                component="label"
-                container
-                alignItems="center"
-                spacing={2}
-              >
-                <Grid item>
-                  <Brightness7Icon 
-                    fontSize="small"
-                  />
-                </Grid>
-                <Grid item>
-                  <Switch size="small" checked={props.darkMode} onChange={() => props.setDarkMode(!props.darkMode)} />
-                </Grid>
-                <Grid item>
-                  <Brightness4Icon 
-                    fontSize="small"
-                  />
-                </Grid>
+              component="label"
+              container
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item>
+                <Brightness7Icon fontSize="small"/>
               </Grid>
-              { props.currentUser ? 
-                <>
-                  <Button 
-                    color="primary" 
-                    variant="contained"
-                    onClick={() => setOpenDialog(true)}
-                    aria-label="create a new entry"
+              <Grid item>
+                <Switch size="small" 
+                  checked={props.darkMode}
+                  onChange={() => props.setDarkMode(!props.darkMode)} 
+                />
+              </Grid>
+              <Grid item>
+                <Brightness4Icon 
+                  fontSize="small"
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row-reverse"
+              justify="flex-start"
+              alignItems="center"
+            >
+            { props.currentUser ? 
+              <>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
                   >
-                    New Entry
+                    <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    horizontal: 'right'
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem component={ Link } to={'/profile'} onClick={handleClose}>
+                    Profile
+                  </MenuItem>
+                  <MenuItem component={ Link } to={'/logout'} onClick={handleClose, props.logout}>
+                    Logout
+                  </MenuItem>
+                </Menu>
+                <Button 
+                  color="primary" 
+                  variant="contained"
+                  onClick={() => setOpenDialog(true)}
+                  aria-label="create a new entry"
+                >
+                  New Entry
+                </Button>
+              </>
+            :
+              <>
+                <Link to={'/register'} aria-label="register">
+                  <Button>
+                    Register
                   </Button>
-                  <IconButton
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                    >
-                      <AccountCircle />
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      verticle: 'top',
-                      horizontal: 'right'
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={handleClose}
-                  >
-                    <MenuItem component={ Link } to={'/profile'} onClick={handleClose}>
-                      Profile
-                    </MenuItem>
-                    <MenuItem component={ Link } to={'/logout'} onClick={handleClose, props.logout}>
-                      Logout
-                    </MenuItem>
-                  </Menu>
-                </>
-              :
-                <>
-                  <Link to={'/register'} aria-label="register">
-                    <Button>
-                      Register
-                    </Button>
-                  </Link>
-                  <Link to={'/login'} aria-label="login">
-                    <Button>
-                      Login
-                    </Button>
-                  </Link>
-                </>
-              }
+                </Link>
+                <Link to={'/login'} aria-label="login">
+                  <Button>
+                    Login
+                  </Button>
+                </Link>
+              </>
+            }
+          </Grid>
         </Toolbar>
       </AppBar>
       <NewEntryDialog
