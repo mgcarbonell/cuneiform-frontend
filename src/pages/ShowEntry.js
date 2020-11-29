395a318c23eeec29c23ad04ac87089ff471bb574
mgcarbonell <mgcarbonell@gmail.com>
2020-11-28 21:15:20 -0800

    Merge prep

--- src/pages/ShowEntry.js
+++ src/pages/ShowEntry.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button, IconButton, Grid } from '@material-ui/core';
 import EntryModel from '../models/entry';
 import EditIcon from '@material-ui/icons/Edit';
